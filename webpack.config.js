e863127c2292d02183244604a96fc20f6484640e
Valerie <kekvalery@gmail.com>
2024-03-28 22:25:33 +0300

    feat: add webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,37 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require("webpack");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: path.resolve(__dirname, "src", "index.js"),
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "build"),
     clean: true,
   },
+  devtool: "inline-source-map",
+  devServer: {
+    port: 9000,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
+      inject: true,
     }),
     new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
   ],
 };
